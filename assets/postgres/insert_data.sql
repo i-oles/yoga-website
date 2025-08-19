e2119ced070884befeedffd6e0b1138177cca3eb
Igor Oles <igor.oles@websensa.com>
2025-08-19 15:09:23 +0200

    confirmation_final_email.tmpl

--- assets/postgres/insert_data.sql
+++ assets/postgres/insert_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO classes (id, day_of_week, start_time, class_level, class_category, current_capacity, max_capacity, location)
-VALUES ('a81bc81b-dead-4e5d-abff-90865d1e13b1', 'Wednesday', '2025-06-28 18:00', 'intermediate', 'vinyasa flow', 1,1,'Ogród Saski');
+VALUES ('a81bc81b-dead-4e5d-abff-90865d1e13b1', 'Środa', '2025-06-28 18:00', 'intermediate', 'vinyasa flow', 1,1,'Ogród Saski');
 
 INSERT INTO classes (id, day_of_week, start_time, class_level, class_category, current_capacity, max_capacity, location)
-VALUES ('d81bc81b-dead-4e5d-abff-90865d1e13b1', 'Friday', '2025-06-30 18:00', 'beginner', 'vinyasa flow', 5, 5,'Ogród Saski');
+VALUES ('d81bc81b-dead-4e5d-abff-90865d1e13b1', 'Piątek', '2025-06-30 18:00', 'beginner', 'vinyasa flow', 5, 5,'Ogród Saski');
 
 INSERT INTO classes (id, day_of_week, start_time, class_level, class_category, current_capacity, max_capacity, location)
-VALUES ('c81bc81b-dead-4e5d-abff-90865d1e13b1', 'Monday', '2025-05-15 18:00', 'beginner', 'ashtanga', 5,5, 'Ogród Saski');
+VALUES ('c81bc81b-dead-4e5d-abff-90865d1e13b1', 'Poniedziałek', '2025-05-15 18:00', 'beginner', 'ashtanga', 5,5, 'Ogród Saski');
 
