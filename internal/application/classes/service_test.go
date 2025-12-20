f216b9d8379d502727a2775da7310276108525b0
Igor Oles <igor.oles@websensa.com>
2025-12-20 11:32:13 +0100

    service create class tests

--- internal/application/classes/service_test.go
+++ internal/application/classes/service_test.go
@@ -16,6 +16,129 @@ import (
 	"github.com/google/uuid"
 )
 
+var (
+	now         = time.Now()
+	pastTime    = now.Add(-2 * time.Hour)
+	futureTime1 = now.Add(1 * time.Hour)
+	futureTime2 = now.Add(2 * time.Hour)
+	futureTime3 = now.Add(3 * time.Hour)
+	testID1     = uuid.New()
+	testID2     = uuid.New()
+	testID3     = uuid.New()
+	testID4     = uuid.New()
+)
+
+var expiredAndFutureClassesWithCurrentCap = []models.ClassWithCurrentCapacity{
+	{
+		ID:              testID1,
+		StartTime:       pastTime,
+		ClassLevel:      "Beginner",
+		ClassName:       "Morning Yoga",
+		CurrentCapacity: 9,
+		MaxCapacity:     10,
+		Location:        "Studio A",
+	},
+	{
+		ID:              testID2,
+		StartTime:       futureTime1,
+		ClassLevel:      "Intermediate",
+		ClassName:       "Afternoon Yoga",
+		CurrentCapacity: 14,
+		MaxCapacity:     15,
+		Location:        "Studio B",
+	},
+	{
+		ID:              testID3,
+		StartTime:       futureTime2,
+		ClassLevel:      "Advanced",
+		ClassName:       "Evening Yoga",
+		CurrentCapacity: 11,
+		MaxCapacity:     12,
+		Location:        "Studio C",
+	},
+	{
+		ID:              testID4,
+		StartTime:       futureTime3,
+		ClassLevel:      "Beginner",
+		ClassName:       "Night Yoga",
+		CurrentCapacity: 19,
+		MaxCapacity:     20,
+		Location:        "Studio D",
+	},
+}
+
+var expiredAndFutureClasses = []models.Class{
+	{
+		ID:          testID1,
+		StartTime:   pastTime,
+		ClassLevel:  "Beginner",
+		ClassName:   "Morning Yoga",
+		MaxCapacity: 10,
+		Location:    "Studio A",
+	},
+	{
+		ID:          testID2,
+		StartTime:   futureTime1,
+		ClassLevel:  "Intermediate",
+		ClassName:   "Afternoon Yoga",
+		MaxCapacity: 15,
+		Location:    "Studio B",
+	},
+	{
+		ID:          testID3,
+		StartTime:   futureTime2,
+		ClassLevel:  "Advanced",
+		ClassName:   "Evening Yoga",
+		MaxCapacity: 12,
+		Location:    "Studio C",
+	},
+	{
+		ID:          testID4,
+		StartTime:   futureTime3,
+		ClassLevel:  "Beginner",
+		ClassName:   "Night Yoga",
+		MaxCapacity: 20,
+		Location:    "Studio D",
+	},
+}
+
+var validClass = models.Class{
+	ID:          testID1,
+	StartTime:   futureTime1,
+	ClassLevel:  "Beginner",
+	ClassName:   "Vinyasa",
+	MaxCapacity: 5,
+	Location:    "Studio A",
+}
+
+var futureClasses = []models.Class{
+	{
+		ID:          testID2,
+		StartTime:   futureTime1,
+		ClassLevel:  "Intermediate",
+		ClassName:   "Ashtanga",
+		MaxCapacity: 15,
+		Location:    "Studio B",
+	},
+	{
+		ID:          testID3,
+		StartTime:   futureTime2,
+		ClassLevel:  "Advanced",
+		ClassName:   "Vinyasa",
+		MaxCapacity: 12,
+		Location:    "Studio C",
+	},
+}
+
+var expiredClass = models.Class{
+	ID:          testID1,
+	StartTime:   pastTime,
+	ClassLevel:  "Beginner",
+	ClassName:   "Vinyasa",
+	MaxCapacity: 5,
+	Location:    "Studio A",
+}
+
 func intPtr(v int) *int {
 	return &v
 }
@@ -100,7 +223,7 @@ func (m *mockBookingsRepo) List(_ context.Context) ([]models.Booking, error) {
 	return []models.Booking{}, nil
 }
 
-func (m *mockBookingsRepo) ListByClassID(_ context.Context, classID uuid.UUID) ([]models.Booking, error) {
+func (m *mockBookingsRepo) ListByClassID(_ context.Context, _ uuid.UUID) ([]models.Booking, error) {
 	return []models.Booking{}, nil
 }
 
@@ -139,90 +262,6 @@ func (m *mockClassesRepoError) Update(_ context.Context, _ uuid.UUID, _ map[stri
 }
 
 func TestService_ListClasses(t *testing.T) {
-	now := time.Now()
-	pastTime := now.Add(-2 * time.Hour)
-	futureTime1 := now.Add(1 * time.Hour)
-	futureTime2 := now.Add(2 * time.Hour)
-	futureTime3 := now.Add(3 * time.Hour)
-	testID1 := uuid.New()
-	testID2 := uuid.New()
-	testID3 := uuid.New()
-	testID4 := uuid.New()
-
-	testClassesWithCurrentCap := []models.ClassWithCurrentCapacity{
-		{
-			ID:              testID1,
-			StartTime:       pastTime,
-			ClassLevel:      "Beginner",
-			ClassName:       "Morning Yoga",
-			CurrentCapacity: 9,
-			MaxCapacity:     10,
-			Location:        "Studio A",
-		},
-		{
-			ID:              testID2,
-			StartTime:       futureTime1,
-			ClassLevel:      "Intermediate",
-			ClassName:       "Afternoon Yoga",
-			CurrentCapacity: 14,
-			MaxCapacity:     15,
-			Location:        "Studio B",
-		},
-		{
-			ID:              testID3,
-			StartTime:       futureTime2,
-			ClassLevel:      "Advanced",
-			ClassName:       "Evening Yoga",
-			CurrentCapacity: 11,
-			MaxCapacity:     12,
-			Location:        "Studio C",
-		},
-		{
-			ID:              testID4,
-			StartTime:       futureTime3,
-			ClassLevel:      "Beginner",
-			ClassName:       "Night Yoga",
-			CurrentCapacity: 19,
-			MaxCapacity:     20,
-			Location:        "Studio D",
-		},
-	}
-
-	testClasses := []models.Class{
-		{
-			ID:          testID1,
-			StartTime:   pastTime,
-			ClassLevel:  "Beginner",
-			ClassName:   "Morning Yoga",
-			MaxCapacity: 10,
-			Location:    "Studio A",
-		},
-		{
-			ID:          testID2,
-			StartTime:   futureTime1,
-			ClassLevel:  "Intermediate",
-			ClassName:   "Afternoon Yoga",
-			MaxCapacity: 15,
-			Location:    "Studio B",
-		},
-		{
-			ID:          testID3,
-			StartTime:   futureTime2,
-			ClassLevel:  "Advanced",
-			ClassName:   "Evening Yoga",
-			MaxCapacity: 12,
-			Location:    "Studio C",
-		},
-		{
-			ID:          testID4,
-			StartTime:   futureTime3,
-			ClassLevel:  "Beginner",
-			ClassName:   "Night Yoga",
-			MaxCapacity: 20,
-			Location:    "Studio D",
-		},
-	}
-
 	tests := []struct {
 		name                string
 		onlyUpcomingClasses bool
@@ -237,69 +276,69 @@ func TestService_ListClasses(t *testing.T) {
 			name:                "List classes without filters",
 			onlyUpcomingClasses: false,
 			classesLimit:        nil,
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
-			wantClasses:         testClassesWithCurrentCap,
+			wantClasses:         expiredAndFutureClassesWithCurrentCap,
 		},
 		{
 			name:                "List only upcoming classes",
 			onlyUpcomingClasses: true,
 			classesLimit:        nil,
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses: []models.ClassWithCurrentCapacity{
-				testClassesWithCurrentCap[1],
-				testClassesWithCurrentCap[2],
-				testClassesWithCurrentCap[3],
+				expiredAndFutureClassesWithCurrentCap[1],
+				expiredAndFutureClassesWithCurrentCap[2],
+				expiredAndFutureClassesWithCurrentCap[3],
 			},
 		},
 		{
 			name:                "List all classes with limit",
 			onlyUpcomingClasses: false,
 			classesLimit:        intPtr(2),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses: []models.ClassWithCurrentCapacity{
-				testClassesWithCurrentCap[0],
-				testClassesWithCurrentCap[1],
+				expiredAndFutureClassesWithCurrentCap[0],
+				expiredAndFutureClassesWithCurrentCap[1],
 			},
 		},
 		{
 			name:                "List upcoming classes with limit",
 			onlyUpcomingClasses: true,
 			classesLimit:        intPtr(2),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses: []models.ClassWithCurrentCapacity{
-				testClassesWithCurrentCap[1],
-				testClassesWithCurrentCap[2],
+				expiredAndFutureClassesWithCurrentCap[1],
+				expiredAndFutureClassesWithCurrentCap[2],
 			},
 		},
 		{
 			name:                "List upcoming classes with limit larger than available",
 			onlyUpcomingClasses: true,
 			classesLimit:        intPtr(10),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses: []models.ClassWithCurrentCapacity{
-				testClassesWithCurrentCap[1],
-				testClassesWithCurrentCap[2],
-				testClassesWithCurrentCap[3],
+				expiredAndFutureClassesWithCurrentCap[1],
+				expiredAndFutureClassesWithCurrentCap[2],
+				expiredAndFutureClassesWithCurrentCap[3],
 			},
 		},
 		{
 			name:                "List classes with limit larger than available",
 			onlyUpcomingClasses: false,
 			classesLimit:        intPtr(10),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
-			wantClasses:         testClassesWithCurrentCap,
+			wantClasses:         expiredAndFutureClassesWithCurrentCap,
 		},
 		{
 			name:                "List upcoming classes with zero limit",
 			onlyUpcomingClasses: true,
 			classesLimit:        intPtr(0),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses:         []models.ClassWithCurrentCapacity{},
 		},
@@ -307,7 +346,7 @@ func TestService_ListClasses(t *testing.T) {
 			name:                "List classes with zero limit",
 			onlyUpcomingClasses: false,
 			classesLimit:        intPtr(0),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses:         []models.ClassWithCurrentCapacity{},
 		},
@@ -331,7 +370,7 @@ func TestService_ListClasses(t *testing.T) {
 			name:                "try to list past classes with upcoming filter",
 			onlyUpcomingClasses: true,
 			classesLimit:        nil,
-			classesRepo:         newMockClassesRepo([]models.Class{testClasses[0]}),
+			classesRepo:         newMockClassesRepo([]models.Class{expiredAndFutureClasses[0]}),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses:         []models.ClassWithCurrentCapacity{},
 		},
@@ -339,10 +378,10 @@ func TestService_ListClasses(t *testing.T) {
 			name:                "List upcoming classes with limit of one",
 			onlyUpcomingClasses: true,
 			classesLimit:        intPtr(1),
-			classesRepo:         newMockClassesRepo(testClasses),
+			classesRepo:         newMockClassesRepo(expiredAndFutureClasses),
 			bookingsRepo:        newMockBookingsRepoWithOneBooking(),
 			wantClasses: []models.ClassWithCurrentCapacity{
-				testClassesWithCurrentCap[1],
+				expiredAndFutureClassesWithCurrentCap[1],
 			},
 		},
 		{
@@ -399,6 +438,91 @@ func TestService_ListClasses(t *testing.T) {
 
 			if !reflect.DeepEqual(classes, tt.wantClasses) {
 				t.Errorf("Expected: %v, got %v", tt.wantClasses, len(classes))
+			}
+		})
+	}
+}
+
+func TestService_CreateClasses(t *testing.T) {
+	tests := []struct {
+		name        string
+		classes     []models.Class
+		classesRepo repositories.IClasses
+		want        []models.Class
+		wantError   bool
+		error       error
+	}{
+		{
+			name:        "Create one valid class",
+			classes:     []models.Class{validClass},
+			classesRepo: newMockClassesRepo([]models.Class{validClass}),
+			want:        []models.Class{validClass},
+		},
+		{
+			name:        "Create valid classes",
+			classes:     futureClasses,
+			classesRepo: newMockClassesRepo(futureClasses),
+			want:        futureClasses,
+		},
+		{
+			name:      "Validation error - expired class",
+			classes:   []models.Class{expiredClass},
+			wantError: true,
+			error: errs.ErrClassValidation(
+				fmt.Errorf("class start_time: %v expired", expiredClass.StartTime),
+			),
+		},
+		{
+			name:      "Validation error - all class should start in future",
+			classes:   expiredAndFutureClasses,
+			wantError: true,
+			error: errs.ErrClassValidation(
+				fmt.Errorf("class start_time: %v expired", expiredAndFutureClasses[0].StartTime),
+			),
+		},
+		{
+			name:        "Repository insert error",
+			classes:     []models.Class{validClass},
+			classesRepo: &mockClassesRepoError{},
+			wantError:   true,
+			error:       fmt.Errorf("could not insert classes: %w", errors.New("db error")),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sender := &mockSender{}
+
+			classesRepo := tt.classesRepo
+			if classesRepo == nil {
+				classesRepo = newMockClassesRepo([]models.Class{})
+			}
+
+			bookingsRepo := newMockBookingsRepoWithOneBooking()
+
+			service := NewService(classesRepo, bookingsRepo, sender)
+			ctx := context.Background()
+
+			result, err := service.CreateClasses(ctx, tt.classes)
+
+			if tt.wantError {
+				if err == nil {
+					t.Fatalf("expected error %v, got nil", tt.error)
+				}
+
+				if !strings.Contains(err.Error(), tt.error.Error()) {
+					t.Fatalf("expected error to contain %q, got %v", tt.error.Error(), err)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if !reflect.DeepEqual(result, tt.want) {
+				t.Errorf("expected: %v, got %v", tt.want, result)
 			}
 		})
 	}
