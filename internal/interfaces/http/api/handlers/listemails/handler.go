2919ce2b4b9d2d4a6b7f492db1b5a9655b9c14cd
Igor Oles <igor.oles@websensa.com>
2026-06-15 23:55:44 +0200

    endpoint

---
+++ internal/interfaces/http/api/handlers/listemails/handler.go
@@ -0,0 +1,50 @@
+package listemails
+
+import (
+	"net/http"
+
+	"main/internal/domain/repositories"
+	apiErrs "main/internal/interfaces/http/api/errs"
+
+	"github.com/gin-gonic/gin"
+)
+
+type handler struct {
+	bookingsRepo    repositories.IBookings
+	apiErrorHandler apiErrs.IErrorHandler
+}
+
+func NewHandler(
+	bookingsRepo repositories.IBookings,
+	apiErrorHandler apiErrs.IErrorHandler,
+) *handler {
+	return &handler{
+		bookingsRepo:    bookingsRepo,
+		apiErrorHandler: apiErrorHandler,
+	}
+}
+
+func (h *handler) Handle(ginCtx *gin.Context) {
+	ctx := ginCtx.Request.Context()
+
+	allBookings, err := h.bookingsRepo.List(ctx)
+	if err != nil {
+		h.apiErrorHandler.Handle(ginCtx, err)
+
+		return
+	}
+
+	uniqueEmails := make(map[string]struct{}, len(allBookings))
+
+	for _, booking := range allBookings {
+		uniqueEmails[booking.Email] = struct{}{}
+	}
+
+	emails := make([]string, 0, len(uniqueEmails))
+
+	for email := range uniqueEmails {
+		emails = append(emails, email)
+	}
+
+	ginCtx.JSON(http.StatusOK, emails)
+}
